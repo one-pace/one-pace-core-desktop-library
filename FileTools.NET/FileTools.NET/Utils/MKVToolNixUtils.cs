a56df00bc35536e5c3b0b64c9d93fbb64182fe64
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-05-11 22:20:46 +0200

    5

--- FileTools.NET/FileTools.NET/Utils/MKVToolNixUtils.cs
+++ FileTools.NET/FileTools.NET/Utils/MKVToolNixUtils.cs
@@ -7,25 +7,27 @@ namespace FileTools.NET.Utils
 {
     public static class MKVToolNixUtils
     {
-        public static void Multiplex(IList<FileInfo> sourceFiles, IList<string> languages, IList<FileInfo> attachments, FileInfo chapterFile, string output)
+        public static void Multiplex(FileInfo videoFile, FileInfo subtitleFile, IList<string> languages, IList<FileInfo> attachments, FileInfo chapterFile, string output)
         {
-            if (sourceFiles == null || sourceFiles.Count == 0)
+            languages = languages.Select(i => i.ToLower()).ToList();
+            string videoExtension = (videoFile?.Extension ?? "").ToLower();
+            if (videoExtension != ".mp4" && videoExtension != ".mkv" && videoExtension != ".m4v" && videoExtension != ".wmv")
             {
-                throw new ArgumentException("No source files specified.");
+                throw new ArgumentException("No video file specified.");
             }
             if (languages == null || languages.Count == 0)
             {
                 throw new ArgumentException("No languages specified.");
             }
-            if (sourceFiles.Count != languages.Count)
+            if (languages.Count != 3)
             {
-                throw new ArgumentException("Not as many source files as languages specified.");
+                throw new ArgumentException("Exactly three languages must be specified.");
             }
             if (output == null)
             {
                 throw new ArgumentException("No output file specified.");
             }
-            if (output.EndsWith(".mkv"))
+            if (!output.EndsWith(".mkv"))
             {
                 throw new ArgumentException("Output file must be an .mkv.");
             }
@@ -33,16 +35,9 @@ namespace FileTools.NET.Utils
             {
                 throw new ArgumentException("Chapter file must be an .xml.");
             }
-            var options = new List<string>();
-            for (int i = 0; i < sourceFiles.Count; i++)
-            {
-                FileInfo sourceFile = sourceFiles[i];
-                string language = languages[i];
-                options.Add("\"" + sourceFile + "\" -a -d --language 0:" + language);
-            }
             string attachFiles = string.Join(" ", attachments.Select(i => "--attach-file \"" + i + "\""));
             string chapters = chapterFile != null ? "--chapters \"" + chapterFile + "\"" : "";
-            string arguments = $"-o {output} " + string.Join(" ", options) + " " + attachFiles + " " + chapters;
+            string arguments = $"-o \"{output}\" --language 0:" + languages[0] + " --language 1:" + languages[1] + " \"" + videoFile + "\" --language 0:" + languages[2] + " \"" + subtitleFile + "\" " + attachFiles + " " + chapters;
             try
             {
                 ProcessUtils.Start("mkvmerge", arguments, true);
