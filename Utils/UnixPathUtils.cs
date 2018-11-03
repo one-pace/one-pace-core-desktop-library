8258cdd04ff9d2fba600ac391d7c61fa450e4fc3
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-11-03 22:04:17 +0100

    Rename FTPPathUtils -> UnixPathUtils, add SanitizePath method

---
+++ Utils/UnixPathUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace OnePaceCore.Utils
+{
+    public static class UnixPathUtils
+    {
+        public static string Combine(params object[] paths)
+        {
+            string joinedPaths = string.Join("/", paths);
+            string replacedSlashes = SanitizePath(joinedPaths);
+            return replacedSlashes;
+        }
+        public static string SanitizePath(string path)
+        {
+            Regex regex = new Regex(@"(?<!:)(\/{2,})");
+            return regex.Replace(path, "/");
+        }
+    }
+}
