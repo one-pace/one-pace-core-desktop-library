a4527ff5366f08d2c9fa56d6a94611177f5c926e
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-11-06 19:38:47 +0100

    Stylecop indentation

--- SonyVegas140/MakeVeg140.cs
+++ SonyVegas140/MakeVeg140.cs
@@ -1,30 +1,30 @@
 namespace ScriptPortal.Vegas.MakeVeg140
 {
-    public class EntryPoint
-    {
-        public void FromVegas(Vegas vegas)
-        {
-            Media media = vegas.Project.MediaPool.AddMedia(Script.Args.ValueOf("media"));
+	public class EntryPoint
+	{
+		public void FromVegas(Vegas vegas)
+		{
+			Media media = vegas.Project.MediaPool.AddMedia(Script.Args.ValueOf("media"));
 
-            foreach (MediaStream stream in media.Streams)
-            {
-                if (stream is VideoStream)
-                {
-                    VideoTrack t = vegas.Project.AddVideoTrack();
-                    VideoEvent e = t.AddVideoEvent(new Timecode(), media.Length);
-                    e.ResampleMode = VideoResampleMode.Disable;
-                    e.AddTake(stream);
-                }
-                else if (stream is AudioStream)
-                {
-                    AudioTrack t = vegas.Project.AddAudioTrack();
-                    AudioEvent e = t.AddAudioEvent(new Timecode(), media.Length);
-                    e.AddTake(stream);
-                }
-            }
+			foreach (MediaStream stream in media.Streams)
+			{
+				if (stream is VideoStream)
+				{
+					VideoTrack t = vegas.Project.AddVideoTrack();
+					VideoEvent e = t.AddVideoEvent(new Timecode(), media.Length);
+					e.ResampleMode = VideoResampleMode.Disable;
+					e.AddTake(stream);
+				}
+				else if (stream is AudioStream)
+				{
+					AudioTrack t = vegas.Project.AddAudioTrack();
+					AudioEvent e = t.AddAudioEvent(new Timecode(), media.Length);
+					e.AddTake(stream);
+				}
+			}
 
-            vegas.SaveProject(Script.Args.ValueOf("output"));
-            vegas.Exit();
-        }
-    }
+			vegas.SaveProject(Script.Args.ValueOf("output"));
+			vegas.Exit();
+		}
+	}
 }
