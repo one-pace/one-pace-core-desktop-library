c128a5d0fd798951b56e546ba6e722a5ae25eccc
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-11-06 19:15:49 +0100

    PathEscaper

---
+++ Utils/PathEscaper.cs
@@ -0,0 +1,15 @@
+﻿namespace OnePaceCore.Utils
+{
+    public static class PathEscaper
+    {
+        /// <summary>
+        /// Escapes a path wrapped inside an argument string. Replaces <c>\</c> with <c>\\</c> and <c>:</c> with <c>\:</c>.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string EscapeWrappedPath(string path)
+        {
+            return path.Replace(@"\", @"\\").Replace(":", @"\:");
+        }
+    }
+}
