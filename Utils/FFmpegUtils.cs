5a574c6fd0aa070898b2c41f78312679681e563d
Simon Haga <1498414+sewil@users.noreply.github.com>
2018-12-17 07:21:03 +0100

    Add ffmpegutils

---
+++ Utils/FFmpegUtils.cs
@@ -0,0 +1,13 @@
+﻿using FileTools.NET.Utils;
+
+namespace OnePaceCore.Utils
+{
+    public static class FFmpegUtils
+    {
+        public static void Encode(string input, string output, string arguments)
+        {
+            string ffmpegArguments = string.Format("-i \"{0}\" {2} \"{1}\"", input, output, arguments);
+            ProcessUtils.Start("ffmpeg", ffmpegArguments);
+        }
+    }
+}
