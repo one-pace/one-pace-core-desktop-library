f02ad97e594d9ab2a59d0fd431db53b867ce25da
Simon Haga <1498414+sewil@users.noreply.github.com>
2020-07-24 23:28:49 +0200

    Update MKVToolNixUtils.cs

--- Utils/MKVToolNixUtils.cs
+++ Utils/MKVToolNixUtils.cs
@@ -7,6 +7,48 @@ namespace OnePaceCore.Utils
 {
     public static class MKVToolNixUtils
     {
+        public static void Multiplex(string[] files, string[] languages, IList<FileInfo> attachments, string chapterFile, string[] subtitleAppendices, string output)
+        {
+            if (output == null)
+            {
+                throw new ArgumentException("No output file specified.");
+            }
+            string outputPath = output;
+            if (!output.EndsWith(".mkv"))
+            {
+                outputPath += ".mkv";
+            }
+            string chapterFilePath = chapterFile;
+            if (chapterFile != null && !chapterFile.EndsWith(".xml"))
+            {
+                chapterFilePath += ".xml";
+            }
+            string arguments = $"--output {EscapePath(output)}";
+            if (files != null)
+            {
+                for (int i = 0; i < files.Length; i++)
+                {
+                    arguments += $" {EscapePath(files[i])}";
+                }
+            }
+            arguments += $" --language 0:{languages[0]} --language 1:{languages[1]} {EscapePath(videoFile)}";
+            arguments += $" --language 0:{languages[2]} ( {EscapePath(subtitleFile)}";
+            if (subtitleAppendices != null && subtitleAppendices.Length > 0)
+            {
+                arguments += " " + string.Join(" ", subtitleAppendices.Select(i => EscapePath(i)));
+            }
+            arguments += " )";
+            if (chapterFile != null)
+            {
+                arguments += $" --chapter-language und --chapters {EscapePath(chapterFile)}";
+            }
+            if (attachments?.Count > 0)
+            {
+                arguments += " " + string.Join(" ", attachments.Select(i => "--attach-file " + EscapePath(i)));
+            }
+
+            ProcessUtils.Start("mkvmerge", arguments);
+        }
         public static void Multiplex(FileInfo videoFile, FileInfo subtitleFile, IList<string> languages, IList<FileInfo> attachments, FileInfo chapterFile, string[] subtitleAppendices, string output)
         {
             languages = languages.Select(i => i.ToLower()).ToList();
