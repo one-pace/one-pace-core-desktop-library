a0d6cf0e348ad544e028cb6a33d0113e76af50cc
Simon Haga <1498414+sewil@users.noreply.github.com>
2021-01-24 16:45:04 +0100

    Add ControlExtensions

---
+++ Extensions/ControlExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace OnePaceCore.Extensions
+{
+    public static class ControlExtensions
+    {
+        public static void Enable(this Control control)
+        {
+            control.Foreground = Brushes.Black;
+            control.IsEnabled = true;
+        }
+        public static void Disable(this Control control)
+        {
+            control.Foreground = Brushes.Gray;
+            control.IsEnabled = false;
+        }
+    }
+}
